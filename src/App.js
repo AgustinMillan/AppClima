94745c5253a43d99e0f03643e9ed8eae2adab1ad
AgustinMillan <victor.agustin.millan@gmail.com>
2022-08-12 18:18:48 -0300

    fixed

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App() {
 
   function onSearch(ciudad) {
     fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`
     )
       .then((r) => r.json())
       .then((recurso) => {
